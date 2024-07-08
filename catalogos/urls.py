aa25a58e19e0bd3ed940fff81353defa64c2b9d1
Dani1804 <da.lizama@duocuc.cl>
2024-07-07 21:13:49 -0400

    backend completado

--- catalogos/urls.py
+++ catalogos/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
 	path('home', views.home, name="home"),
+    path('catalogoLibros', views.catalogoLibros, name="catalogoLibros"),
+    path('catalogoAutores', views.catalogoAutores, name="catalogoAutores"),
+    path('categorias', views.categorias, name="categorias")
 ]
--- catalogos/views.py
+++ catalogos/views.py
@@ -1,7 +1,42 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def home (request):
-    context = {}
+    Navbars = NavItem.objects.all()
+    context = {
+        "Navbars": Navbars,
+        
+    }
+    
     return render(request,'catalogos/home.html',context)
+
+def catalogoLibros (request):
+    Navbars = NavItem.objects.all()
+    Libros = Libro.objects.all()
+    context = {
+        "Navbars": Navbars,
+        "Libros": Libros  
+    }
+    return render(request,'catalogos/catalogoLibros.html',context)
+
+def catalogoAutores (request):
+    Navbars = NavItem.objects.all()
+    Autores = Autor.objects.all()
+    context = {
+        "Navbars": Navbars,
+        "Autores": Autores
+        
+    }
+    return render(request,'catalogos/catalogoAutores.html',context)
+
+def categorias (request):
+    Navbars = NavItem.objects.all()
+    Categorias = Categoria.objects.all()
+    context = {
+        "Navbars": Navbars,
+        "Categorias": Categorias
+        
+    }
+    return render(request,'catalogos/categorias.html',context)
