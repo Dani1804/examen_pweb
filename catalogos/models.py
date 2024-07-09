bcac682b0e6c0714850747160d5e37fb397153cb
Dani1804 <da.lizama@duocuc.cl>
2024-07-09 19:39:22 -0400

    Base de datos corregida con relaciones

--- catalogos/models.py
+++ catalogos/models.py
@@ -1,15 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Libro(models.Model):
-    id_libro = models.AutoField(db_column="idLibro", primary_key=True)
-    titulo = models.CharField(max_length=200)
-    ano_de_publicacion = models.IntegerField()
-    descripcion_breve = models.TextField()
-
-    def __str__(self):
-        return self.titulo
-
 class Autor(models.Model):
     id_autor = models.AutoField(db_column="idAutor",primary_key=True)
     nombre = models.CharField(max_length=100)
@@ -25,6 +16,16 @@ class Categoria(models.Model):
 
     def __str__(self):
         return self.nombre
+class Libro(models.Model):
+    id_libro = models.AutoField(db_column="idLibro", primary_key=True)
+    titulo = models.CharField(max_length=200)
+    ano_de_publicacion = models.IntegerField()
+    descripcion_breve = models.TextField()
+    id_autor = models.ForeignKey(Autor, on_delete=models.CASCADE, related_name='libros')
+    id_categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name='libros')
+
+    def __str__(self):
+        return self.titulo
 
 class NavItem(models.Model):
     id_navbar = models.AutoField(db_column="idNavbar", primary_key=True)
